48814eeb1eea7e3fc5620d3e09671e13dcf9c2d4
Kyle Lejeune <kylejl98@gmail.com>
2023-05-10 23:58:10 -0400

    Finished calculator

--- calculator.js
+++ calculator.js
@@ -1,33 +1,75 @@
 const rs = require('readline-sync');
+
+const addition = (numbers) => {
+  console.log(`${numbers[0]} + ${numbers[1]} = ${numbers[0] + numbers[1]}`)
+}
+
+const subtraction = (numbers) => {
+  console.log(`${numbers[0]} - ${numbers[1]} = ${numbers[0] - numbers[1]}`)
+}
+
+const multiplication = (numbers) => {
+  console.log(`${numbers[0]} * ${numbers[1]} = ${numbers[0] * numbers[1]}`)
+}
+
+const division = (numbers) => {
+  console.log(`${numbers[0]} / ${numbers[1]} = ${numbers[0] / numbers[1]}`)
+}
+
 const operations = [
-    {symbol: '+', operation: 'Addition'},
-    {symbol: '-', operation: 'Subtraction'},
-    {symbol: '*', operation: 'Multiplication'},
-    {symbol: '/', operation: 'Division'},
+  {symbol: '+', operation: 'Addition', function: (numbers) => addition(numbers)},
+  {symbol: '-', operation: 'Subtraction', function: (numbers) => subtraction(numbers)},
+  {symbol: '*', operation: 'Multiplication', function: (numbers) => multiplication(numbers)},
+  {symbol: '/', operation: 'Division', function: (numbers) => division(numbers)},
 ]
 
 const getOperation = () => {
-    console.log(`\nWhat kind of operation would you like to perform?\nYou can choose from:`)
-      operations.map((listItem) => {
-            console.log(`    ${listItem.symbol} -> ${listItem.operation}`)
-        })
-        
-    let selectedOperation = rs.question(`>> `);
+  console.log(`\nWhat kind of operation would you like to perform?\nYou can choose from:`)
+    operations.map((listItem) => {
+        console.log(`    ${listItem.symbol} -> ${listItem.operation}`)
+      })
+      
+  let selectedOperation = rs.question(`>> `);
+  let operationCheck = operations.find(operation => operation.symbol == selectedOperation);
 
-    if (operations.find(operation => operation.symbol == selectedOperation) != selectedOperation) {
-        console.log(`  '${selectedOperation}' was not recognized. Please try again!`)
-        selectedOperation = getOperation()
-    }
+  if (operationCheck == undefined || operationCheck.symbol != selectedOperation) {
+    console.log(`  '${selectedOperation}' was not recognized. Please try again!`)
+    selectedOperation = getOperation()
+  }
 
-    console.log(`  '${selectedOperation}' was selected`)
-    return selectedOperation;
+  return selectedOperation;
 }
 
+const getNumbers = () => {
+  let numbers = []
+  
+  console.log(`Please enter the first number in your operation`)
+  numbers[0] = rs.questionFloat(`>> `);
+  console.log(`Please enter the second number in your operation`)
+  numbers[1] = rs.questionFloat(`>> `);
 
+  return numbers;
+  
+}
 
+const main = () => {
+  
+  let selectedOperation = getOperation()
+  
+  console.log(`You have selected ${operations.filter(operation => operation.symbol == selectedOperation)[0].operation}`)
+  
+  // Prompt the user to select an operator
+  operations.find(operation => operation.symbol == selectedOperation).function(getNumbers());
 
-// Print welcome message and prompt user
-console.log(`==========================`);
-console.log(`Welcome to the calculator!`);
+  if(rs.keyInYN(`Would you like to perform another calculation?`)){
+    main()
+  }
+}
 
-getOperation();
+console.clear()
+console.log(`======================================================================`);
+console.log(`                      Welcome to the calculator!                      `);
+main();
+console.log(`\n\n\n`)
+console.log(`                 Thank you for using the calculator!                  `);
+console.log(`======================================================================`);
